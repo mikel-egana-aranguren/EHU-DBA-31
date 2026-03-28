c2d3f383ab164ba3ea67b6da4d1282846bfc5622
Mikel Egaña Aranguren <mikel.egana.aranguren@gmail.com>
2026-03-28 19:09:48 +0100

    SPARQL

--- SPARQL/adibideak/post.sh
+++ SPARQL/adibideak/post.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl -X POST http://localhost:7200/repositories/ABD-01_25-26_ejemplos_RDF \
+curl -X POST http://localhost:7200/repositories/DBK-31-25-26-SPARQL \
   -H "Content-Type: application/sparql-query" \
   -H "Accept: application/sparql-results+json" \
   --data-binary "@triples.rq"
